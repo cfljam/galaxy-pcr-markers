39f0a3697270fbfd654f5bd787b59a2e9e1eb1ae
cfljam <john.mccallum@plantandfood.co.nz>
2013-07-23 03:08:00 +1200

    Added duckpunch for check_output in run_P3

--- run_p3.py
+++ run_p3.py
@@ -7,6 +7,25 @@ import subprocess as sp
 import copy
 
 
+
+import subprocess
+
+if "check_output" not in dir( subprocess ): # duck punch it in!
+    def f(*popenargs, **kwargs):
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed, it will be overridden.')
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise CalledProcessError(retcode, cmd)
+        return output
+    subprocess.check_output = f
+
+
 ##call P3 with dict of args, returns dict, no exception handling 
 def run_P3(target_dict):
     p3_str=''
