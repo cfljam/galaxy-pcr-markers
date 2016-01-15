1767a0e5204a2ddb49e08648b5d8e3cf78eacb95
John McCallum <cfljam@users.noreply.github.com>
2016-01-16 11:42:13 +1300

    commot before tweaking design

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   url = 'https://github.com/cfljam/galaxy-pcr-markers', #  the URL to the github repo
   download_url = 'https://github.com/cfljam/galaxy-pcr-markers/tarball/1.0' , # Tarball URL
   keywords = ['PCR', 'genetics', 'markers','genomics'], # arbitrary keywords
-  install_requires = ['cython','bcbio-gff','primer3-py'],
+  install_requires = ['biopython','cython','bcbio-gff','primer3-py',],
   scripts=['design_primers.py','vcf2gvf.py','find_CAPS.py'],
   classifiers = [],
 )
