c9b06e4bc9b3c544ae4bc5c7d0e4e777fe544682
John McCallum <cfljam@users.noreply.github.com>
2016-01-16 07:27:57 +1300

    setup for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'PCRMarkerDesign',
+  packages = ['PCRMarkerDesign'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Bulk PCR Marker Design from NGS',
+  author = 'John McCallum',
+  author_email = 'john.mccallum@plantandfood.co.nz',
+  url = 'https://github.com/cfljam/galaxy-pcr-markers', #  the URL to the github repo
+  download_url = https://github.com/cfljam/galaxy-pcr-markers'/tarball/1.0', # Tarbal URL
+  keywords = ['PCR', 'genetics', 'markers','genomics'], # arbitrary keywords
+  classifiers = [],
+)
