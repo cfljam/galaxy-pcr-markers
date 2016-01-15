2c616289fd499b5216a829ab8416bb82e0f413f4
John McCallum <cfljam@users.noreply.github.com>
2016-01-16 11:07:25 +1300

    sort of working for pip install

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ setup(
   author = 'John McCallum',
   author_email = 'john.mccallum@plantandfood.co.nz',
   url = 'https://github.com/cfljam/galaxy-pcr-markers', #  the URL to the github repo
-  download_url = https://github.com/cfljam/galaxy-pcr-markers'/tarball/1.0', # Tarbal URL
+  download_url = 'https://github.com/cfljam/galaxy-pcr-markers/tarball/1.0' , # Tarball URL
   keywords = ['PCR', 'genetics', 'markers','genomics'], # arbitrary keywords
+  install_requires = ['cython','bcbio-gff','primer3-py'],
+  scripts=['design_primers.py','vcf2gvf.py','find_CAPS.py'],
   classifiers = [],
 )
